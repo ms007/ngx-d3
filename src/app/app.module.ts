a3ad2bea489246db55f010da98c2f7a833ba09fb
CHA <christoph.kaden@opitz-consulting.com>
2018-06-27 11:49:44 +0200

    PieChart: animated transitions, oc-prefix, package details, demo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PieChartModule } from 'projects/pie-chart/src/public_api';
+import { PieChartModule } from 'pie-chart';
+import { PieChartDemoComponent } from './pie-chart-demo/pie-chart-demo.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, PieChartModule],
+  declarations: [
+    AppComponent, 
+    PieChartDemoComponent
+  ],
+  imports: [
+    BrowserModule, 
+    PieChartModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
